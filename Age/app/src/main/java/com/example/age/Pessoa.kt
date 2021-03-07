5e3bbd8b7ddebaa247dc074b8402f5a3c0374836
Rafael <robatistag@indracompany.com>
2021-03-06 23:54:09 -0300

    finalizado

--- Age/app/src/main/java/com/example/age/Pessoa.kt
+++ Age/app/src/main/java/com/example/age/Pessoa.kt
@@ -1,4 +1,18 @@
 package com.example.age
 
-class Pessoa {
+import java.io.Serializable;
+import java.time.Year
+
+class Pessoa: Serializable {
+    var name: String
+    var year: Int
+
+    constructor(name: String, year: Int) {
+        this.name = name
+        this.year = year
+    }
+
+    fun calculateYear(): Int {
+        return 2021 - this.year
+    }
 }
--- Age/app/src/main/java/com/example/age/ShowYear.kt
+++ Age/app/src/main/java/com/example/age/ShowYear.kt
@@ -2,10 +2,22 @@ package com.example.age
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class ShowYear : AppCompatActivity() {
+    private lateinit var etResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_year)
+
+        this.etResult = findViewById<TextView>(R.id.etResult)
+
+        val person = intent.getSerializableExtra("person") as Pessoa
+        var name = person.name
+        var year = person.calculateYear()
+
+        this.etResult.setText("$name, você possui $year anos!")
     }
 }
